5d9330efe7aa18a1436e9135d1b93478524b56a7
OleksandrBohatyrov <120181397+OleksandrBohatyrov@users.noreply.github.com>
2023-08-30 11:41:58 +0300

    Update tabeli_loomine.sql
    kõik

--- tabeli_loomine.sql
+++ tabeli_loomine.sql
@@ -18,3 +18,34 @@ create table reservation(
 
 Alter table reservation
 add foreign key (guest_id) REFERENCES guest(id);
+	
+create table reserved_room(
+	id int not null primary key identity(1,1),
+	number_of_rooms int,
+	room_type_id int,
+	foreign key (room_type_id) references room_type(id));
+
+create table room(
+	id int not null primary key identity(1,1),
+	number varchar(10),
+	name varchar(40),
+	status varchar(10),
+	smoke bit,
+	room_type_id int,
+	foreign key (room_type_id) references room_type(id));
+
+create table occupied_room(
+	id int not null primary key identity(1,1),
+	check_in datetime,
+	check_out datetime,
+	room_id int,
+	reservation_id int,
+	foreign key (room_id) references room(id),
+	foreign key (reservation_id) references reservation(id));
+
+create table hosted_at(
+	id int not null primary key identity(1,1),
+	guest_id int,
+	occupied_room_id int,
+	foreign key (guest_id) references guest(id),
+	foreign key (occupied_room_id) references occupied_room(id));
